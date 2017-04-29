cf68b336b018041ec0e2479b7dc75228e6b374f3
Ereptor <marsimil@gmail.com>
2017-04-29 12:05:11 +0200

    added ratcht key initialization/management (no ratcheting\!)

--- Client/conversation.py
+++ Client/conversation.py
@@ -5,6 +5,7 @@ from threading import Thread
 import urllib2
 import json
 
+import os.path
 import diffie
 from Crypto.Signature import PKCS1_v1_5 as pkcs
 from Crypto.Hash import SHA
@@ -35,7 +36,8 @@ class Conversation:
         ) # message processing loop
         self.msg_process_loop.start()
         self.msg_process_loop_started = True
-        self.ratchet_keypairs = {"name": {"private": "x", "public": "x"}} #CRYPTO
+        self.ratchet_keys = {} #CRYPTO
+        self.my_ratchet_keys = {}
         self.session_keys = {} #CRYPTO 
 
     def append_msg_to_process(self, msg_json):
@@ -140,8 +142,42 @@ class Conversation:
             diffie2 = diffie.derive_shared_secret(signed_secret,
                 int(keys["identity_key"]))
             self.session_keys[participant] = str(diffie1) + str(diffie2)
-        pass
+        conversationkey_path = "conversationkeys_"+str(self.id)+".json"
+        if not os.path.exists(conversationkey_path):
+            self.update_my_ratchet_key(diffie.generate_keys(), 0)
+        else:
+            with open(conversationkey_path, "r") as con_keys:
+                self.my_ratchet_keys = json.load(con_keys)
+        print self.get_my_ratchet_key_for_id(0)
 
+
+    def update_last_ratchet_key(self, user_name, key):
+        """ Update ratchet key list with last seen key for user """
+        self.ratchet_keys[user_name] = key
+
+    def update_my_ratchet_key(self, key, m_id):
+        """
+        key = {"public": x, "private": y}
+        m_id: ratchet key is valid from this message
+        """
+        self.my_ratchet_keys[m_id] = key
+        with open("conversationkeys_"+str(self.id)+".json", "w") as keys_file:
+            json.dump(self.my_ratchet_keys, keys_file)
+
+    def get_my_ratchet_key_for_id(self, m_id):
+        if m_id < 0:
+            return None
+        keys = sorted(self.my_ratchet_keys.keys())
+        for i in range(0, len(keys)):
+            if m_id < keys[i]:
+                return self.my_ratchet_keys[keys[i-1]]
+        return self.my_ratchet_keys[keys[-1]]
+
+    def get_last_ratchet_key(self, user_name):
+        """ Return last ratchet key seen for user (None if it's the first) """
+        if not user_name in self.ratchet_keys:
+            return None
+        return self.ratchet_keys[user_name]
 
     def process_incoming_message(self, msg_raw, msg_id, owner_str):
         '''
