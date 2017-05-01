f779049be9b7ea6f71e5f332a848b7b3a9f765e8
Ereptor <marsimil@gmail.com>
2017-05-01 11:16:42 +0200

    added two new users

--- Server/RegisteredUsers.py
+++ Server/RegisteredUsers.py
@@ -1,5 +1,7 @@
 RegisteredUsers = [
     {"user_name": "Elon", "password": "Musk"},
     {"user_name": "Bill", "password": "Gates"},
-    {"user_name": "Steve", "password": "Jobs"}
+    {"user_name": "Steve", "password": "Jobs"},
+    {"user_name": "Nikola", "password": "Tesla"},
+    {"user_name": "Alan", "password": "Turing"}
 ]
